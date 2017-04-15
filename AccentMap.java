ef0fcede50f8aae8fcf703d8d50af46952f1e18f
InazumaCorp <jivan_khakee@horacemann.org>
2017-04-14 22:47:34 -0400

    small square buttons

--- AccentMap.java
+++ AccentMap.java
@@ -17,19 +17,21 @@ class AccentMap extends JFrame
         buttons.setBounds(0,0,1270,710);
         buttons.setLayout(null);
         Insets insets = pane.getInsets();
-        
+
         /*size.width = size.height = Math.max(size.width, size.height);
         setPreferredSize(size);
         setContentAreaFilled(false);*/
-
-        JButton england = new JButton("England");
-        buttons.add(england);
-        Dimension size = england.getPreferredSize();
-        england.setBounds(600 + insets.left, 200 + insets.top, size.width, size.height);
         
-        JButton france = new JButton("France");
+
+        JButton england = new JButton("");
+        buttons.add(england);
+        england.setPreferredSize(new Dimension(10, 10));
+        Dimension size = england.getPreferredSize();
+        england.setBounds(618 + insets.left, 250 + insets.top, size.width, size.height);
+
+        JButton france = new JButton("");
         buttons.add(france);
-        france.setBounds(50 + insets.left, 50 + insets.top, size.width, size.height);
+        france.setBounds(630 + insets.left, 280 + insets.top, size.width, size.height);
 
         pane.add(map, new Integer(1));
         pane.add(buttons, new Integer(2));
